cf6866c911a482aa58d1ed4b33a162e0d7b0ba95
John Victor Carreon <victor@lanexus.com>
2020-03-01 21:53:19 +0800

    Added necessary packages and theme styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "antd";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button type="primary">Button</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
